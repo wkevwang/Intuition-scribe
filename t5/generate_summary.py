d9797148c5595e8c5681dd2e9638b3854f33b19c
Kevin Wang <weixiang@ualberta.ca>
2020-12-02 14:09:28 -0800

    Integrate T5 summarizer into scribe.py

---
+++ t5/generate_summary.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import torch
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__))) # Import current folder to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Import parent folder to sys.path
+
+from utils import *
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = None
+
+
+def init_model(model_name, model_type='t5-large', checkpoints_dir='checkpoints'):
+    global model
+    model = initialize_t5_model(model_type)
+    load_model(model, model_name, checkpoints_dir)
+    model = model.to(device)
+
+
+def summarize(question, answer, max_len=170):
+    prompt = build_prompt(question, answer)
+    summary = generate(model, prompt, max_len=max_len, device=device)
+    return summary
